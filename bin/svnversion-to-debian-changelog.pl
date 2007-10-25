b3b3e3039a485da2f170d3afdacad3e1c5978fd5
tf <none@none>
2007-10-25 17:27:25 +0000

    More work on improved web distribution system
    --HG--
    branch : www
    rename : debian/packages/nsim-0.1/debian/changelog => debian/packages/nsim/debian/changelog
    rename : debian/packages/nsim-0.1/debian/control => debian/packages/nsim/debian/control
    rename : debian/packages/nsim-0.1/debian/copyright => debian/packages/nsim/debian/copyright
    rename : debian/packages/nsim-0.1/debian/files => debian/packages/nsim/debian/files
    rename : debian/packages/nsim-0.1/debian/rules => debian/packages/nsim/debian/rules
    extra : convert_revision : svn%3A4be47922-762e-0410-ae34-bcde03179581/www%404447

--- bin/svnversion-to-debian-changelog.pl
+++ bin/svnversion-to-debian-changelog.pl
@@ -7,20 +7,10 @@ my $ver=shift;
 
 my $changelog_file="debian/packages/nsim-$ver/debian/changelog";
 
-my $changelog=`cat $changelog_file`;
-
 my $svnversion=`cd tmp/nmag-$ver/nsim/; svnversion`;
 
 $svnversion=~/^(\d+)/ or die "Fatal: svnversion is '$svnversion'\n";
 $svnversion=$1;
-
-
-$changelog=~/^(\S+)\s*\(\d+\.\d+\.(\d+)\)/ or die "Bad changelog file!\n";
-$svnversion eq $1 and do
-  {
-    warn "SVN Version number did not change. No adjustment necessary.\n";
-    exit(0);
-  };
 
 warn "SVN Version: $svnversion\n";
 
@@ -29,20 +19,18 @@ my @months=qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 
 my @time=localtime(time());
 
-my $extra_changelog=sprintf qq^nsim (0.1.$svnversion) testing; urgency=low
+my $changelog=sprintf qq^nsim ($ver.$svnversion) testing; urgency=low
 
-  * Automatic version sync with SVN
+  * SVN-version-aligned autobuild of debian package
 
  -- The Nmag team <nmag\@soton.ac.uk>  %s, %02d %s %d %02d:%02d:%02d +0000
 
 ^,$days[$time[6]], $time[3], $months[$time[4]],1900+$time[5],
   $time[2], $time[1], $time[0];
 
-my $new_changelog="$extra_changelog$changelog";
-
 open OUT, ">$changelog_file" and do
   {
-    print OUT $new_changelog;
+    print OUT $changelog;
     close OUT;
   };
 
