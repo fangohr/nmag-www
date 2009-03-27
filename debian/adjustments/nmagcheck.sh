3a09cb8e8628993f79d6ea1aaee7cf4b0d869dd7
fangohr <none@none>
2009-03-27 12:11:26 +0000

    Extended the range of tests we can carry out on debian-based installations.
    --HG--
    branch : www
    extra : convert_revision : svn%3A4be47922-762e-0410-ae34-bcde03179581/www%406130

--- debian/adjustments/nmagcheck.sh
+++ debian/adjustments/nmagcheck.sh
@@ -1,16 +1,47 @@
 #!/bin/sh
-tmpdir=/tmp/nmag_check_`date +"%Y-%m-%d-%H-%M-%S"`
+tmpdir=/tmp/nmagcheck/`date +"%Y-%m-%d-%H-%M-%S"`
 echo $tmpdir
 mkdir -p $tmpdir
 echo "Which version do we use for the tests?:"
 /usr/bin/nsimversion
 echo "Will run one nmag regression test in $tmpdir"
-echo "(starting in 5 seconds)."
-sleep 5
+#echo "(starting in 1 seconds)."
+sleep 1
 
 cp -a /usr/lib/nsim/interface $tmpdir
+mkdir -p $tmpdir/bin
+cp -a /usr/bin/nsim $tmpdir/bin
 chmod -R u+w $tmpdir
-cd $tmpdir/interface/nmag/manual/example1
-/usr/bin/pytest_nsim
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "Testing interface/nsim"
+echo "#"
+echo "#"
+cd $tmpdir/interface/nsim
+/usr/bin/pytest_nsim -k "-test_slow" 
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "Testing interface/nmag"
+echo "#"
+echo "#"
+cd $tmpdir/interface/nmag
+/usr/bin/pytest_nsim -k "-test_slow" 
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "#"
+echo "Quick tests completed. You may run 'rm -rf $tmpdir' to free some space on /tmp"
+echo "Starting extended tests in 30 seconds (press CTRL+C to stop)"
+sleep 30
+
+cd $tmpdir/interface/nmag
+/usr/bin/pytest_nsim 
 
 echo "Tests completed. You may run 'rm -rf $tmpdir' to free some space on /tmp"
